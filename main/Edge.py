adf32b1e6f10b2ffd27a880076d76a91bf7e5adc
Faizan <chfaizan.510@outlook.com>
2025-06-10 15:02:19 +0100

    Added a check that validates the instance of the first parameter of an object of this class to be of the class 'Node'.

--- main/Edge.py
+++ main/Edge.py
@@ -1,5 +1,12 @@
+from main.Node import Node
+from main.DataTypeError import DataTypeError
+
 class Edge:
     def __init__(self, starting_node, ending_node, travel_distance):
+        # explicitly check that 'starting_node' is of type 'Node'
+        if not isinstance(starting_node, Node):
+            raise DataTypeError(f"Parameter 'starting_node' should be of type 'Node', but got a different type", None, Node, type(starting_node))
+
         self.startNode = starting_node
         self.endNode = ending_node
         self.distance = travel_distance
