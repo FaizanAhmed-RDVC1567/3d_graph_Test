a86c094554136d4b638d40a9607ecf599c06d23f
Faizan <chfaizan.510@outlook.com>
2025-06-10 15:04:04 +0100

    Added a new test.

--- test/Edge_class_negative_test.py
+++ test/Edge_class_negative_test.py
@@ -1,9 +1,23 @@
 import unittest
+from main.Node import Node
+from main.Edge import Edge
+from main.DataTypeError import DataTypeError
 
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+    def test_Edge_wrong_first_parameter(self):
+        #test1_node = Node("test1", 0)
+        #test2_node = Node("test2", 0)
+        #test1_edge = Edge("", test2_node, 40)
+        #test2_edge = Edge(test2_node, test1_node, 40)
+        with self.assertRaises(DataTypeError) as e_dt:
+            test1_node = Node("test1", 0)
+            test2_node = Node("test2", 0)
+            test1_edge = Edge("", test2_node, 40)
+            test2_edge = Edge(test2_node, test1_node, 40)
+        self.assertEqual(str(e_dt.exception),
+                         f"Parameter 'starting_node' should be of type 'Node', but got a different type | Expected: {Node}, "
+                         f"Received: {type("")}\nError Code: {""}")
 
 
 if __name__ == '__main__':
